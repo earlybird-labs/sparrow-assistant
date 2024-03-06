044ffdd10b728d7a49798fc2b5c8ac3b7951d2ce
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-03-05 22:35:57 -0500

    Add BLE functionality to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,68 @@
 #include <Arduino.h>
-#include "BluetoothSerial.h" // Header file for Bluetooth
 
-// Check if BluetoothSerial library is available
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
-BluetoothSerial ESP_BT; // Object for Bluetooth
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
 
-void setup() {
-  pinMode(2, OUTPUT);
-  Serial.begin(921600);
-  Serial.println("Hello World!");
+#define SERVICE_UUID        "aee41206-d70d-47f0-9a4f-b9f4faba2a28"
+#define CHARACTERISTIC_UUID "dd83e3eb-324f-46a7-a79c-981c40f2401b"
 
-  // Bluetooth device name
-  ESP_BT.begin("ESP32_BT_Test"); // Init Bluetooth with device name "ESP32_BT_Test"
-  Serial.println("Bluetooth device is ready to pair");
+
+class MyCallbacks: public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    std::string value = pCharacteristic->getValue();
+
+    if (value.length() > 0)
+    {
+      Serial.println("*********");
+      Serial.print("New value: ");
+      for (int i = 0; i < value.length(); i++)
+      {
+        Serial.print(value[i]);
+      }
+
+      Serial.println();
+      Serial.println("*********");
+    }
+  }
+};
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.println("1- Download and install an BLE scanner app in your phone");
+  Serial.println("2- Scan for BLE devices in the app");
+  Serial.println("3- Connect to ESP32-BLE_Server");
+  Serial.println("4- Go to CUSTOM CHARACTERISTIC in CUSTOM SERVICE and write something");
+  Serial.println("5- See the magic =)");
+
+  BLEDevice::init("EARLYBIRD-ESP32-BLE-Server");
+  BLEServer *pServer = BLEDevice::createServer();
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  pCharacteristic->setValue("Hello World");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
 }
 
-void loop() {
-  delay(1000);
-  digitalWrite(2, HIGH);
-  // Check if we receive anything from Bluetooth
-  if (ESP_BT.available()) {
-    String msg = ESP_BT.readString(); // Read what we receive
-    Serial.println(msg); // Print it out
-    ESP_BT.println("Received: " + msg); // Echo back to Bluetooth
-  }
-  delay(5000);
-  digitalWrite(2, LOW);
+void loop()
+{
+  delay(2000);
 }
