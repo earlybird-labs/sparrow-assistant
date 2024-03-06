ce134abe37de44b781b18491d4cd0afc19d84bec
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-03-06 02:44:59 -0500

    Refactor AudioManager class to improve code readability and add isRecording method

--- src/Audio/AudioManager.h
+++ src/Audio/AudioManager.h
@@ -13,9 +13,10 @@ public:
     void stopRecording();
     void streamAudio();
     std::vector<uint8_t> getAudioData(); // Method to get recorded audio data
+    bool isRecording();                  // Method to check if audio is currently being recorded
 
 private:
-    bool isRecording = false;
+    bool _isRecording = false;
     std::vector<uint8_t> audioData; // Buffer to store audio data
     void initI2S();
     void clearAudioData(); // Helper method to clear the audio buffer
